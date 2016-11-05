f9a0fa5932aecae321d0cd637121298f5cff701c
aanilku <aanilku@ncsu.edu>
2016-11-05 18:22:01 -0400

    setters and getters

--- src/TestClass2.java
+++ src/TestClass2.java
@@ -9,7 +9,20 @@ public class TestClass2 {
 		this.name = name;
 		this.salary = salary;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
 
+	
 	
 	
 }
